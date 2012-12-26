be5f793290c288b50bb71c0f8707397071782276
wamberg <wamberg@technushi.com>
2012-12-26 18:41:42 -0500

    Use requirejs to shim jquery, underscore, and backbone.

---
+++ app/scripts/app.js
@@ -0,0 +1,7 @@
+define(['jquery', 'underscore', 'backbone'],
+       function() {
+    console.log('global jQuery', $);
+    console.log('global underscore', _);
+    console.log('global Backbone', Backbone);
+  }
+);
