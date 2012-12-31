96f9c74a0cb7e6c50fc506025815ee920ca1efe8
wamberg <wamberg@technushi.com>
2012-12-31 14:28:30 -0500

    Add PhoticRouter index route.

--- app/scripts/routes/photic-router.js
+++ app/scripts/routes/photic-router.js
@@ -1,8 +1,23 @@
 define(
-  ['backbone'],
-  function(Backbone) {
+  ['backbone', 'views/photic-view'],
+  function(Backbone, PhoticView) {
 
     var PhoticRouter = Backbone.Router.extend({
+
+      initialize: function(opt) {
+        this.el = opt.el;
+      },
+
+      routes: {
+        "": "index"
+      },
+
+      index: function() {
+        var view = new PhoticView();
+        this.el.empty();
+        this.el.append(view.render({el: this.el}))
+      }
+
     });
 
     return PhoticRouter;
--- test/spec/integration.js
+++ test/spec/integration.js
@@ -1,14 +1,33 @@
-define(['models/slide-model', 'chai', 'mocha'], function(SlideModel, chai) {
-  var tests = function() {
+define(
+  ['routes/photic-router', 'chai', 'mocha', 'jquery'],
+  function(PhoticRouter, chai) {
+    var tests = function() {
 
-    assert = chai.assert;
+      assert = chai.assert;
 
-    describe('SlideCollection', function() {
-      describe('Creation', function() {
+      describe('Photic Router', function() {
+
+        var router;
+
+        beforeEach(function() {
+          router = new PhoticRouter({el: $('#photic')});
+        })
+
+        describe('Routes', function() {
+
+          it('has routes', function() {
+            assert.isObject(router.routes, 'router.routes is object');
+          });
+
+          it('has index', function() {
+            assert.propertyVal(router.routes, '', 'index', 'router has index route');
+          });
+
+        });
       });
-    });
 
-  };
+    };
 
-  return tests
-});
+    return tests
+  }
+);
