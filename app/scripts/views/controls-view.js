676ef6f5c7565b11674a137a825292527d5aa281
wamberg <wamberg@technushi.com>
2013-01-07 18:27:44 -0500

    Add click event handler ControlsView#nextSlide.

--- app/scripts/views/controls-view.js
+++ app/scripts/views/controls-view.js
@@ -2,11 +2,26 @@ define(
   [
     'text!templates/controls.html',
     'backbone',
-    'handlebars'
+    'handlebars',
+    'underscore'
   ],
   function(controlsTemplate) {
 
     var ControlsView = Backbone.View.extend({
+
+      initialize: function() {
+        _.bindAll(this, 'render', 'nextSlide');
+      },
+
+      events: {
+        'click .icon-forward': 'nextSlide'
+      },
+
+      nextSlide: function(evt) {
+        evt.preventDefault();
+        var next = this.model.getNextSlide();
+        this.model.setCurrentSlide(next);
+      },
 
       template: Handlebars.compile(controlsTemplate),
 
