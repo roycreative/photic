a036140b6da918246ceace84868f37e8ef4b510b
wamberg <wamberg@technushi.com>
2013-01-22 19:07:54 -0500

    Play sound and display time elapsed when play button is clicked.

--- app/scripts/views/audio-view.js
+++ app/scripts/views/audio-view.js
@@ -9,7 +9,14 @@ define(
     var AudioView = Backbone.View.extend({
 
       initialize: function() {
-        _.bindAll(this, 'render', 'audioSrc');
+        _.bindAll(
+          this,
+          'advanceProgressBar',
+          'audioSrc',
+          'playAudio',
+          'progressBar',
+          'render'
+        );
         this.model.bind('playAudio', this.playAudio);
         this.model.bind('pauseAudio', this.pauseAudio);
       },
@@ -21,9 +28,17 @@ define(
         return this;
       },
 
-      audio: this.$('#audio'),
+      advanceProgressBar: function(e) {
+          this.progressBar().html(e.target.currentTime);
+      },
+
+      audio: _.memoize(function() {return document.getElementById('audio');}),
+
+      progressBar: _.memoize(function() {return this.$('#progress-bar');}),
 
       playAudio: function() {
+        this.audio().addEventListener('timeupdate', this.advanceProgressBar, false);
+        this.audio().play();
       },
 
       pauseAudio: function() {
