56d281967692108cada741e40512007fb2dc848e
wamberg <wamberg@technushi.com>
2013-02-03 15:05:10 -0500

    Changing the AudioProgressView impacts the audio's currentTime.

--- app/scripts/views/audio-progress-view.js
+++ app/scripts/views/audio-progress-view.js
@@ -1,26 +1,36 @@
 define(
   [
     'handlebars',
-    'scripts/views/base-view',
+    'scripts/views/base-audio-view',
     'text!scripts/templates/audio-progress.html',
     'underscore'
   ],
-  function(Handlebars, BaseView, audioProgressTemplate, _) {
-    var AudioProgressView = BaseView.extend({
+  function(Handlebars, BaseAudioView, audioProgressTemplate, _) {
+    var AudioProgressView = BaseAudioView.extend({
       initialize: function() {
         _.bindAll(
           this,
           'audioLen',
+          'changeAudioTime',
           'render',
           'updateProgressBar'
         );
         this.model.bind('audioTimeUpdate', this.updateProgressBar);
       },
 
+      events: {
+        'change #progressBar': 'changeAudioTime'
+      },
+
       audioLen: function() { return this.model.get('audioLen'); },
 
       updateProgressBar: function(currentTime) {
         this.progressBar().val(currentTime);
+      },
+
+      changeAudioTime: function(evt) {
+        var seekedTime = this.progressBar().val();
+        this.audio().currentTime = seekedTime;
       },
 
       progressBar: function() { return this.$('#progressBar'); },
